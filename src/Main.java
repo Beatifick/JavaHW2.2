f9a1f5cdf43f6dbaa5943e408cef54522d35aa4a
Beatific <1telpenkov1@mail.ru>
2025-10-22 10:28:21 +0300

    main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         int wallet = 100;
-        int refill = 1100;
+        int refill = 2200;
         int bonus;
 
         if (refill >= 1000) {
-            bonus = refill/100;
-            System.out.println("Полученный бонус = " + bonus);
+            bonus = refill / 100;
+                System.out.println("Полученный бонус = " + bonus);
             System.out.println("В кошельке: " + (wallet + refill + bonus));
         } else {
             bonus = 0;
